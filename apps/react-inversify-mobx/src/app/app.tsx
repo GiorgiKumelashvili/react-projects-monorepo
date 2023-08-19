9d635554a44fe5bced315f77329b5131b9cf18e2
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2023-08-19 18:05:40 +0400

    initial

---
+++ apps/react-inversify-mobx/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="react-inversify-mobx" />
+    </div>
+  );
+}
+
+export default App;
